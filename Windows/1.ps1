cc10e9b0b7f39f20589181a27d0700a7e25fc6ba
Nate Fortner <nefortner@outlook.com>
2016-10-04 11:11:21 -0700

    Fixes

--- Windows/1.ps1
+++ Windows/1.ps1
@@ -4,7 +4,7 @@ $ADSIComp = [adsi]"WinNT://$Computername"
 $administrators = [adsi]"$ADSIComp/Administrators,group"
 $a = new-object -comobject wscript.shell
 foreach ($user in $users) {
-    echo $user
+    #echo $user
     $username = $user.Name
     $del = $a.popup("Do you want to remove "+$username+"?", 0,"Delete Files",4+256+32)
 #    $del = Read-Host "Would you like to remove" $username"?"
@@ -30,30 +30,49 @@ foreach ($user in $users) {
     }
 }
 $users = Get-WmiObject -Class Win32_UserAccount
-echo $users
+#echo $users
 $addUsers = $a.popup("Do you want to add any users?", 0,"Delete Files",4+256+32)
 if($addUsers -eq "6")
 {
-    [Array]$users
+    [Array]$newUsers = @()
     [int]$numUsers = Read-Host "How many users would you like to add? (Enter only numbers in this field)"
-    echo $numUsers
+    #echo $numUsers
     for ($i = 0; $i -lt $numUsers; $i++)
     {
         $i++
         $user = New-Object –TypeName PSObject
-        $username = Read-Host "Username for user number" $i":"
+        [String]$username = Read-Host "Username for user number" $i":"
         $password = Read-Host -AsSecureString "Password for user number" $i":"
         $group = Read-Host "Group for user number" $i":"
         $user | Add-Member –MemberType NoteProperty –Name username –Value $username 
         $user | Add-Member –MemberType NoteProperty –Name password –Value $password
         $user | Add-Member –MemberType NoteProperty –Name group –Value $group
-        $users+= $user
+        $newUsers+= $user
         $i--
     }
-    echo $users
-    foreach ($userinfo in $users)
+    #echo $users
+    foreach ($userinfo in $newUsers)
     {
+        #$CompObject = [ADSI]"WinNT://$Computer"
+        #$NewObj = $CompObject.Create("$ObjectType",$ObjectName)
+        echo $userinfo.username
+        
         $user = $ADSIComp.Create("User",$userinfo.username)
+        $BSTR = [system.runtime.interopservices.marshal]::SecureStringToBSTR($userinfo.password)
+
+        $_password = [system.runtime.interopservices.marshal]::PtrToStringAuto($BSTR)
+        #Set password on account 
+
+        $user.SetPassword($_password)
+
+        $user.SetInfo()
+
+
+        #Cleanup 
+
+        [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR) 
+
+        Remove-Variable Password,BSTR,_password 
         
     }
 }
