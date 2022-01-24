66c4c611a61d61ea3b5e0257b57e7eb5579d5cd5
Nate Fortner <bsasealteam6.com>
2022-01-24 11:25:12 -0800

    Adding auto pull

---
+++ pull.php
@@ -0,0 +1,9 @@
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+shell_exec( ‘cd /config/www/CyberPatriots/ && git reset –hard HEAD && git pull’ );
+}
+
+?>hi
